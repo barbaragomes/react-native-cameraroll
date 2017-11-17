f14f6644433cbe4eb01f85e4bd8221c70a2ac007
Barbara Gomes <barbaramaltagomes@gmail.com>
2017-11-16 18:20:52 -0800

    Adding createJSModules for older react-native versions

--- android/src/main/java/com/chrisbianca/cameraroll/RNCameraRollPackage.java
+++ android/src/main/java/com/chrisbianca/cameraroll/RNCameraRollPackage.java
@@ -11,6 +11,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNCameraRollPackage implements ReactPackage {
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
